8a24abf0d3954b502902e80533771c88ba4908a1
ellis882 <ellisrachel1304@me.com>
2022-02-16 19:34:55 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KTsPjECsQnpiYKrqHjLZEUEyzi6MYqfInvD8Tid4KhggOvNXlFyuAPM6H8IiXSWfAjQaETqYqy4odpPV2jZbAW100WSNL3PyW',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
